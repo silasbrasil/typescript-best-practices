e88c9eeed4d20a027b74e4829b0f5f877ee437a6
Silas Brasil <silas.swf@gmail.com>
2025-01-21 17:51:31 -0300

    add ts-pattern, create some examples and update README.md

--- README.md
+++ README.md
@@ -1,9 +1,21 @@
 # TypeScript Best Practices
 
+## Installing packages
+
+```bash
+$ npm i
+```
+
 To run each file use `node version 22.7.0`+ and run follow command
 
 ```bash
 $ node --experimental-strip-types --experimental-transform-types {FILE NAME}.ts 
+```
+
+Or use `version 23.6.0`
+
+```bash
+$ npx tsx {FILE NAME}.ts
 ```
 
 Or use `ts-node`
---
+++ pattern-matching.ts
@@ -0,0 +1,24 @@
+import { match, P } from 'ts-pattern'
+
+
+/**Check types */
+const toString = (value: unknown): string =>
+  match(value)
+    .with(P.string, (str) =>  str)
+    .with(P.number, (num) =>  num.toFixed(0))
+    .with(P.boolean, (bool) => `${bool}`)
+    .otherwise(() => 'Unknown')
+
+console.log(toString(true))
+console.log(toString(23))
+console.log(toString('My text'))
+
+
+/**Prevent null values to number type */
+const handleNullValues = (value: number | null | undefined): string =>
+  match(value)
+    .with(P.nonNullable, () => 'it is a number')
+    .otherwise(() => 'it is either null or undefined')
+
+console.log(handleNullValues(null))
+console.log(handleNullValues(334))
